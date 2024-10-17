7323ddce01be40adca6d4c19e8ebffbc531c6b47
ntandomabuto <ntandomabuto@gmail.com>
2024-10-17 15:17:16 +0200

    nav

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -18,11 +18,6 @@
     </ul>
     </div>
         </div>
-        <!-- <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/resume">Resume</router-link> |
-        <router-link to="/projects">Projects</router-link> |
-        <router-link to="/contact">Contact</router-link>  -->
         
     </nav>
 </template>
